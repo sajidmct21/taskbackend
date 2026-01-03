57e0d1b2fb16fcdb4ee664906749bb24fd655302
sajidmct21 <sajidmct21@gmail.com>
2026-01-03 14:49:40 +0500

    nbb

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -143,7 +143,7 @@ export const updateImportantTask = asyncHandler(async (req, res, next) => {
   const taskData = Task.findById(id);
   const impTask = taskData.important;
   await Task.findByIdAndUpdate(id, { important: !impTask });
-  res.status(200).json(new ApiResponse(200, "Task is updated", impTask));
+  res.status(200).json(new ApiResponse(200, "Task is updated", impTask.toString()));
 });
 
 // update complete Task
