cc18d7bef67b6d908c4027ade63424469a19112b
sajidmct21 <sajidmct21@gmail.com>
2026-01-04 20:31:19 +0500

    ghgjgh

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ env.config();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://your-frontend.vercel.app"
+  "https://your-frontend.vercel.app",
+  "https://taskfrontend-three.vercel.app/"
 ];
 
 app.use(cors({
