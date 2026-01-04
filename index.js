67bf9f8bed3e969c4d3e9bb8fa7be168a51d3f3d
sajidmct21 <sajidmct21@gmail.com>
2026-01-04 20:37:07 +0500

    hjklhj

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
