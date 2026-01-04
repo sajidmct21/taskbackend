d75b8a2548c44163764781fe1be4b02c7ba96d8d
sajidmct21 <sajidmct21@gmail.com>
2026-01-04 20:54:19 +0500

    dsddf

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ const allowedOrigins = [
   "https://taskfrontend-three.vercel.app"
 ];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 // app.use(cors({
 //   origin: true,
@@ -39,11 +39,7 @@ const allowedOrigins = [
 // app.options("*", cors());
 // app.use(express.json());
 
-app.use(cors({
-  origin: 'https://taskfrontend-three.vercel.app', // Allow only your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'] // Specify allowed headers
-}));
+
 
 // Middlewares
 app.use(express.json());
